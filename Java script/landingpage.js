273e002ed6450570393fde481d198572a6a16c29
amanshu0143 <aman2000.am@gmail.com>
2024-12-12 18:04:25 +0530

    Update landingpage.js

--- Java script/landingpage.js
+++ Java script/landingpage.js
@@ -15,7 +15,7 @@ var swiper = new Swiper(".mySwiper", {
   var swiper = new Swiper(".mySwiper1", {
     slidesPerView: 2.5,
    
-    spaceBetween: 5,
+    spaceBetween: 0,
     grabCursor: true,
     autoplay: {
       delay: 6500,
